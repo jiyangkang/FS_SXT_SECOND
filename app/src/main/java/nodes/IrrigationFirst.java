7b366b5c6eec0d2f865262f98c93c9533fae0dc0
jiyangkang <jiyangkang@sohu.com>
2016-04-09 15:49:13 +0800

    2016年4月9日

---
+++ app/src/main/java/nodes/IrrigationFirst.java
@@ -0,0 +1,22 @@
+package nodes;
+
+/**
+ * Created by jiyangkang on 2016/4/9 0009.
+ */
+public class IrrigationFirst extends Node{
+    public volatile static IrrigationFirst irrigationFirst;
+
+    private IrrigationFirst(){
+
+    }
+
+    public static IrrigationFirst getIrrigationFirst(){
+        if (irrigationFirst == null){
+            synchronized (IrrigationFirst.class){
+                if (irrigationFirst == null)
+                    irrigationFirst = new IrrigationFirst();
+            }
+        }
+        return irrigationFirst;
+    }
+}
